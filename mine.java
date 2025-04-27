2d83cf9c7726564bf1f2d284c72abf91ca3ddae3
ayaalshamali02 <aayah9900@gmail.com>
2025-04-27 17:05:15 +0300

    update

---
+++ mine.java
@@ -0,0 +1,21 @@
+package iloveyou
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class mine {
+	WebDriver driver = EdgeDriver;
+	
+	@b4test
+	public void my setup() {
+		driver.get("https://www.google.com);"
+				+ "
+	}
+
+	
+	@test
+	public void my test () {
+		System.out.println(driver.getTitle());
+		
+	}
+}
